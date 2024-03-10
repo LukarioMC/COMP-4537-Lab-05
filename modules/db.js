4fdfdc0b9e9200c1fa4bc5c0a49c480e885df6ec
Dakaro Mueller <LukarioMC@gmail.com>
2024-03-09 20:01:11 -0800

    Implement GET

--- modules/db.js
+++ modules/db.js
@@ -1,17 +1,20 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
+const url = require('url');
 const { respondJSON } = require('./utils');
 
-const client = new Client(process.env.DATABASE_URL);
+const client = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    max: 32,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+});
 
 async function executeSQL(userQuery) {
-    await client.connect();
     try {
         const result = await client.query(userQuery);
-        return result;
+        return { status: 'ok', result };
     } catch (err) {
-        return err;
-    } finally {
-        client.end();
+        return { status: 'error', err };
     }
 }
 
@@ -33,19 +36,18 @@ async function handleSQLRequest(req, res) {
 }
 
 async function handleGet(req, res) {
-    const parsedAddress = new URL(req.url);
-    let query = parsedAddress.searchParams.get('query')?.trim();
+    const parsedAddress = url.parse(req.url, true);
+    // Get SQL statement/query from URL query string
+    let query = parsedAddress?.query['query']?.trim();
     if (!query) {
         return respondJSON(res, 400, {
             status: 400,
             error: 'Property "query" must be in request query string',
         });
+    } else {
+        result = await executeSQL(query);
+        respondJSON(res, 200, result);
     }
-    query = decodeURIComponent(query); // Decode any URI encoding
-    console.log('Query to run on server: ' + query);
-    result = await executeSQL(query);
-    console.log('Query result from server: ' + result);
-    respondJSON(res, 200, result);
 }
 
 async function handlePost(req, res) {}
